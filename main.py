81571ab78e8a13438d18ced00b218b718316a8d6
AkshaySatasiya <akshaysatasiya2811@gmail.com>
2026-01-17 14:08:52 +0530

    HTML frontend integrtion using Jinja2 Templates

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
         "id":1,
         "author":"Akshay Satasiya",
-        "title":"FastAPI",
-        "content":"Hello World!",
-        "date_posted":"April 20, 2025"
+        "title":"FastAPI is Awesome",
+        "content":"This framework is really eassy to use and super fast.",
+        "date_posted":"Jan 13, 2026"
+    },
+    {
+        "id":2,
+        "author":"Akshay Satasiya",
+        "title":"LLMs",
+        "content":"Large Language Models!",
+        "date_posted":"Jan 14, 2026"
     },
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts":posts, "title": "Home"})
 
 
 @app.get("/api/posts")
